3091c233c977e841e0f9c98e916b6ec287b2674d
Sean Zhang <sezhang@deloitte.com>
2022-11-11 11:37:20 +1100

    random classname

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
-import { BrowserRouter } from 'react-router-dom';
-import { StylesProvider,createGenerateClassName } from '@material-ui/core/styles';
 
-
-const generateClassName =  createGenerateClassName({
+const generateClassName = createGenerateClassName({
   productionPrefix: 'co',
-})
-
+});
 
 export default () => {
   return (
     <BrowserRouter>
       <StylesProvider generateClassName={generateClassName}>
-          <div>
-            <Header />
-            <MarketingApp />
-          </div>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
       </StylesProvider>
     </BrowserRouter>
   );
